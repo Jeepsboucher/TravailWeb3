c4264ea43ef731e7209160f16307e4bdb4664604
B533P-20\533 <alessard97@hotmail.com>
2018-12-02 11:29:40 -0500

    FINAL DEBUG!!

--- View/SessionValide.php
+++ View/SessionValide.php
@@ -2,9 +2,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-if (!isset($_SESSION))
+if (!isset($_SESSION['id']))
 {
     header('Location: index.php');
-    //require 'Connexion.php';
 }
  ?>
